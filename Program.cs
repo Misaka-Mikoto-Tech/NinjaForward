371934d4d0535438bdda13f8ca33e186c08eb2cc
Misaka Mikoto <easy66@live.com>
2025-05-31 07:26:27 +0800

    代码修改

--- Program.cs
+++ Program.cs
@@ -14,19 +14,23 @@ namespace LimitNinjaWorkers
             string? exeDir = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule?.FileName);
             string szConfigPath = $"{exeDir}\\{kConfigName}";
 
-            if (!File.Exists(szConfigPath))
+            // 只有编译时才附加参数，执行其它指令比如 --version 时不能附加
+            if (args.Length > 0 && args[0] == "-C")
             {
-                Console.WriteLine($"Can not find file '{szConfigPath}'");
-            }
-            else
-            {
-                // 给 ninja.exe 附加额外的启动参数
-                szArgsExtra = File.ReadAllText(szConfigPath);
-                szArgs += " ";
-                szArgs += szArgsExtra;
+                if (!File.Exists(szConfigPath))
+                {
+                    //Console.WriteLine($"Can not find file '{szConfigPath}'");
+                }
+                else
+                {
+                    // 给 ninja.exe 附加额外的启动参数
+                    szArgsExtra = File.ReadAllText(szConfigPath);
+                    szArgs += " ";
+                    szArgs += szArgsExtra;
+                }
             }
 
-            Console.WriteLine($"Run Ninja with args:{szArgs}, extra:{szArgsExtra}");
+            //Console.WriteLine($"Run Ninja with args:{szArgs}, extra:{szArgsExtra}");
 
             string szLog = $"{DateTime.Now.ToString()}\t args:{szArgs}, extra:{szArgsExtra}\r\n";
             File.WriteAllText($"{exeDir}\\ninja.log", szLog);
@@ -34,8 +38,21 @@ namespace LimitNinjaWorkers
             Process process = new Process();
             process.StartInfo.FileName = $"{exeDir}\\ninja_ori.exe";
             process.StartInfo.Arguments = szArgs;
-            process.Start();
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            process.OutputDataReceived += (s, ev) => { Console.Write(ev.Data); };
+            process.ErrorDataReceived += (s, ev) => { Console.Write(ev.Data); };
+            process.Exited += (s, ev) => { Console.Write(ev.ToString()); };
             
+            process.Start();
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
             // android studio 结束 ninja 进程时只会结束其直接子进程，这会导致 clang++.exe 无法被结束，我们使用Job来解决
             var job = new JobManagement.Job();
             job.AddProcess(process.Handle);
